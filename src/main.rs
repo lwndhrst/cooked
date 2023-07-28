d19e8cd80079566b23c821aaf381921f39d1880d
Leon Windhorst <leon@windhorst.dev>
2023-07-28 10:20:39 +0200

    feat: serde + ron

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::collections::hash_map::HashMap;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct CookbookEntry {
+    name: String,
+    ingredients: Vec<(String, u8)>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Cookbook {
+    recipes: HashMap<String, CookbookEntry>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cookbook: Cookbook = ron::from_str(include_str!("../cookbook.ron")).unwrap();
+
+    println!("Cookbook: {}", ron::ser::to_string(&cookbook).unwrap());
 }
