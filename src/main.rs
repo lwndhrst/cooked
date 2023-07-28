095184babc4fe2ea48067f19e28702bf8869e97d
Leon Windhorst <leon@windhorst.dev>
2023-07-28 11:10:23 +0200

    feat: recipe selection with clap

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,44 @@
-use std::collections::hash_map::HashMap;
+use clap::Parser;
 use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Deserialize, Serialize)]
-struct CookbookEntry {
-    name: String,
-    ingredients: Vec<(String, u8)>,
-}
+use std::collections::hash_map::HashMap;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Cookbook {
-    recipes: HashMap<String, CookbookEntry>,
+    recipes: HashMap<CookbookKey, CookbookValue>,
+}
+
+#[derive(clap::ValueEnum, Clone, Debug, Deserialize, Serialize, Hash, Eq, PartialEq)]
+enum CookbookKey {
+    ValenciaMeal,
+    Couscous,
+    TeffSandwich,
+    KingOfJungleHamburg,
+    FigPie,
+    DatePalmWine,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct CookbookValue {
+    name: String,
+    ingredients: HashMap<CookbookKey, u8>,
+}
+
+#[derive(Debug, Parser)]
+struct Args {
+    /// The recipe you wish to cook
+    #[clap(value_enum)]
+    recipe: CookbookKey,
+
+    /// Number of cooks
+    #[arg(short)]
+    n: u64,
 }
 
 fn main() {
     let cookbook: Cookbook = ron::from_str(include_str!("../cookbook.ron")).unwrap();
 
-    println!("Cookbook: {}", ron::ser::to_string_pretty(&cookbook, ron::ser::PrettyConfig::default()).unwrap());
+    let args = Args::parse();
+
+    println!("Selected: {:?}", &args.recipe);
+    println!("Cookbook: {:?}", cookbook.recipes[&args.recipe]);
 }
